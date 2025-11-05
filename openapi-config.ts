d630e793819db1ff8bd00760fe435c2163695706
Chip <cstubbs@totalwine.com>
2025-11-05 10:56:25 -0500

    add esbuild-runner

--- openapi-config.ts
+++ openapi-config.ts
@@ -2,9 +2,9 @@ import type { ConfigFile } from '@rtk-query/codegen-openapi'
 
 const config: ConfigFile = {
   schemaFile: 'https://petstore3.swagger.io/api/v3/openapi.json',
-  apiFile: './src/store/emptyApi.ts',
+  apiFile: './app/store/emptyApi.ts',
   apiImport: 'emptySplitApi',
-  outputFile: './src/store/petApi.ts',
+  outputFile: './app/store/petApi.ts',
   exportName: 'petApi',
   hooks: true,
 }
